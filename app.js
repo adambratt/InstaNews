76c4ebdb833d2e9d7c4b8e40195c19feaa74fec2
Adam Bratt <adamjbratt@gmail.com>
2012-04-27 09:01:57 -0400

    handlebar rocker

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var http = require('http'),
     sio = require('socket.io'),
     fs = require('fs'),
-    mongo = require('mongodb'),
     path = require('path');
 
 // Start the server at port 8080
@@ -39,15 +38,44 @@ server.listen(8000);
 // Create a Socket.IO instance, passing it our server
 var io = sio.listen(server);
 
-var i = 3;
-var news = [{id: 1, title: 'A live example', ticker: 'AAPL'}, {id: 2, title: 'More Info', ticker: 'F'}];
+var i = 5;
+var news = {1: {id: 1, title: 'Prosperity Bancshares Reports Q1 EPS $0.77 vs $0.74 Est', ticker: 'PB', date: '8:35:34'}, 2: {id: 2, title: 'Ruth\'s Hospitality Group Reports Q1 EPS $0.15 vs $0.15 Est; Revenues $101M vs $101.52M Est', ticker: 'RUTH', date: '8:36:22'}, 3: {id: 3, title: 'JP Morgan Maintains Amazon at Overweight, Raises PT from $210 to $250', ticker: 'AMZN', date: '8:38:45'}, 4: {id: 4, title: 'Simon Property Group Reports Q1 FFO $1.82 vs $1.68 Est; Revenues $1.12B vs $1.05B Est', ticker: 'SPG', date: '8:39:30'}};
 
 // "Routing" for Socket.IO
 io.sockets.on('connection', function (socket) {
     
+    //Initialize data
+    socket.emit('news', news);
+    
     // When a newsfeed is submitted
     socket.on('add news', function (data) {
         console.log(data);
-        io.sockets.emit('news', {'title': 'something', 'ticker': 'ok', 'date': 'lol'});
-    });                    
+        io.sockets.emit('new', {'title': 'something', 'ticker': 'ok', 'date': 'lol'});
+    });
+    
+    // When new news is created
+    socket.on('create news', function (data) {
+        send = {};
+        i += 1;
+        news[i] = {title: '', ticker: '', date: getTime(), id: i};
+        send[i] = {title: '', ticker: '', date: getTime(), id: i};        
+        io.sockets.emit('news', send);
+        socket.emit('edit news', i);
+    });
+    
+    socket.on('live news', function(data){
+       io.sockets.emit('update news', data); 
+    });
+    
+    socket.on('save news', function(data){
+       io.sockets.emit('close news', data); 
+    });
+    
 });
+
+function getTime(){
+    var d = new Date();
+    var s = (d.getSeconds() < 10) ? "0"+d.getSeconds() : d.getSeconds();
+    var m = (d.getMinutes() < 10) ? "0"+d.getMinutes() : d.getMinutes();
+    return d.getHours()+":"+m+":"+s;
+}
--- public/js/frontend.js
+++ public/js/frontend.js
@@ -10,65 +10,72 @@ function getTime(){
     return d.getHours()+":"+m+":"+s;
 }
 
+function addNews(news){
+    $('.newsfeed').prepend(newsTemplate(news));
+}
 
 
+var newsTemplate = Handlebars.compile($('#newsItemTemplate').html());
 var socket = io.connect('http://localhost');
 
-var NewsItem = function(title, date, ticker){
-    var self = this;
-    
-    // Data
-    self.title = ko.observable(title);
-    self.date = date;
-    self.ticker = ko.observable(ticker);
-    self.editing = ko.observable(false);
-    
-    // Behaviors
-    this.edit = function() { this.editing(true) }
-}
-
-var FeedViewModel = function() {
-    var self = this;
-    
-    // Data
-    self.newsToAdd = ko.observable(new NewsItem("","",""));
-    self.tickerToAdd = ko.observable();
-    self.titleToAdd = ko.observable();
-    self.username = ko.observable();
-    self.news = ko.observableArray([
-        new NewsItem("Bob Jones Uni", "Today", "AAPL"),
-        new NewsItem("This is an itemz", "Today", "AAPL")
-    ]);
-    
-    // Behaviours
-    self.createNews = function(news) {
-      $('.addnews').show();
-      self.newsToAdd = ko.observable(new NewsItem("", getTime(), ""));
-      $('.addnews input').val('');
-      self.news.unshift(self.newsToAdd);
-    };
-    
-    self.addNews = function() {
-        socket.emit('add news', {'title': self.titleToAdd(), 'date': getTime(), 'ticker': self.tickerToAdd()});
-        $('.addnews').hide();
-    }
-    
-    
-}
-
-var editingId = 0;
-
-ko.applyBindings(new FeedViewModel());
 
 socket.on('news', function(data){
-   console.log(data); 
+   for ( x in data ){
+        addNews(data[x]);
+   }
 });
 
-socket.on('updatenews',function(data){
-   $('#id'); 
+socket.on('update news', function(data){
+    console.log(data);
+   var e = $('.news[rel='+data.id+']');
+   e.find('.ticker span').text(data.ticker);
+   e.find('.headline').text(data.title);
+   if (data.username != $('#username').val()){
+        e.find('.istyping').text(data.username+' is typing...');
+        e.find('.istyping').removeClass('hide');
+   }
+});
+
+socket.on('edit news',function(data){
+   var e = $('.news[rel='+data+']');
+   e.find('.ticker span').addClass('hide');
+   e.find('input[name=ticker]').removeClass('hide').focus();
+   e.find('.headline').addClass('hide');
+   e.find('input[name=headline]').removeClass('hide');
+});
+
+socket.on('close news', function(data){
+   var e = $('.news[rel='+data+']');
+   e.find('.istyping').addClass('hide');
+   e.removeClass('green');
 });
 
 $(function(){
    $('#enterUsername').modal();
+   $('#addNewsBtn').click(function(){
+        socket.emit('create news');
+        $(this).addClass('hide');
+        return false;
+   });
+   $('.ticker input, input[name=headline]').live('keyup', function(xx){
+        if(xx.which != 13){           
+            var e = $(this).parents('.news');
+            var news = {'title': e.find('input[name=headline]').val(), 'ticker': e.find('input[name=ticker]').val(), 'id': e.attr('rel'), 'username': $('#username').val()};
+            console.log(news);
+            socket.emit('live news', news);
+        }
+   });
+   $('input[name=headline]').live('keypress', function(e){
+      var x = $(this).parents('.news');
+      
+      if(e.which == 13){
+        socket.emit('save news', x.attr('rel'));
+        x.find('.headline').removeClass('hide');
+        x.find('input[name=headline]').addClass('hide');
+        x.find('.ticker span').removeClass('hide');
+        x.find('input[name=ticker]').addClass('hide');
+        $('#addNewsBtn').show();
+      }
+   });
 });
 
